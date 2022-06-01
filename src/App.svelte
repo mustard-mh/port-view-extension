7596a4834aa6cc12da3156257764b26066cea3ee
mustard <huiwen@gitpod.io>
2022-06-02 03:20:03 +0800

    tailwind support failed

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,29 @@
 <script>
+    import Tailwindcss from "./Tailwind.svelte";
     let time = new Date().toString();
+    let headers = [
+        { label: "Port", field: "port" },
+        { label: "Local Address", field: "local_address" },
+        { label: "Running Process", field: "running_process" },
+        { label: "Visibility", field: "visibility" },
+        { label: "Origin", field: "origin" },
+    ];
+    let ports = [
+        {
+            port: 3000,
+            local_address: "https://github.com/mustard-mh/port-view-extension",
+            running_process: "/usr/bin/node hello.js",
+            visibility: "private",
+            origin: "Auto Forwarded",
+        },
+        {
+            port: 8080,
+            local_address: "http://localhost:8080",
+            running_process: "",
+            visibility: "public",
+            origin: "Auto Forwarded",
+        },
+    ];
     window.addEventListener("message", (event) => {
         if (event.data.command === "changeName") {
             time = event.data.name;
@@ -7,35 +31,15 @@
     });
 </script>
 
-<main>
-    <h1>Hello World!</h1>
-    <p>
-        Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-        how to build Svelte apps.
-    </p>
-    <p>
-        Current time is {time}!
-    </p>
+<main class="text-center">
+    <div class="table-container">
+        <div class="header-container flex">
+            {#each headers as header (header.label)}
+                <div class="header-cell text-green-700">{header.label}</div>
+            {/each}
+        </div>
+    </div>
 </main>
 
 <style>
-    main {
-        text-align: center;
-        padding: 1em;
-        max-width: 240px;
-        margin: 0 auto;
-    }
-
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        font-size: 4em;
-        font-weight: 100;
-    }
-
-    @media (min-width: 640px) {
-        main {
-            max-width: none;
-        }
-    }
 </style>
